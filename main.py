e08e6fc8e7df782dba7fd73aefeffb10818684f9
ZanRafal <01159225@pw.edu.pl>
2021-12-26 12:37:54 +0100

    Dodano logikę blokowania pól i wyświetlania ich w kolorze

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # Rafał Zan
 # 311 214
 
+import Board
+
 def main():
     prepareBoard()
+    play()
+
+
+
+def play():
+    wyświetTablice()
+    wyświetlStatus()
+    dopasujKsztalt(wyświetlKształt())
+    wyswietlRuchy()
