b0532fa28a100a0d7be9189b4b0b4b4976b542c2
ZanRafal <01159225@pw.edu.pl>
2021-12-26 12:38:05 +0100

    Dodano logikę blokowania pól i wyświetlania ich w kolorze

--- Board.py
+++ Board.py
@@ -1,41 +1,60 @@
 import random as rand
+from termcolor import colored
 
 def prepareBoard():
-    initBoard()
-    lockRandomFields()
-    displayBoard()
+    board = initBoard()
+    displayBoard(board)
 
 def initBoard():
     board = []
     newLine = []
+
     for i in range(0, 8):
         for j in range(0, 8):
             newLine.append("X")
         board.append(newLine)
         newLine = []
 
+    #TODO: Zmienna quantity jako parametr
+    board = lockTiles(8, board)
     return board
 
-def displayBoard():
-    table = initBoard()
+def displayBoard(table = []):
     print("|==========================================|")
     print("| X | 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 |")
     print("|---|======================================|")
     for i in range(8):
         print('| {} '.format(i + 1), end="")
         for j in range(8):
-            print('| {} |'.format(table[i][j]), end="")
+            if table[i][j] == "W":
+                print("| " + colored(table[i][j], "red") + " |", end="")
+            elif table[i][j] == "O":
+                print("| " + colored(table[i][j], "green") + " |", end="")
+            else:
+                print("| " + table[i][j] + " |", end="")
         print()
         print("|---|--------------------------------------|")
     print("|==========================================|")
 
-def lockRandomFields(quantity):
+def pickRandomFields(quantity):
     board = []
     newLine = []
     for i in range(0, quantity):
         for j in range(2):
-            newLine.append(rand.randint(0, 8))
+            newLine.append(rand.randint(0, 7))
         board.append(newLine)
-        print(newLine)
         newLine = []
+
     return board
+
+def lockTiles(quantity, myBoard = []):
+    randomFields = pickRandomFields(quantity)
+
+    for i in range(len(randomFields)):
+        y = randomFields[i][0]
+        x = randomFields[i][1]
+        myBoard[x][y] = "W"
+
+    return myBoard
+
+prepareBoard()
