b0e99fcaa9510b49d197ebf45b026de554e92072
ZanRafal <01159225@pw.edu.pl>
2021-12-27 18:10:07 +0100

    Dodano metodę zamiany w kształcie dużego kwadratu

--- Shapes.py
+++ Shapes.py
@@ -27,6 +27,23 @@ def small_square_shape(x, y, tile=[]):
     toggle(x, y - 1, tile)          #upper left
     toggle(x + 1, y - 1, tile)      #upper right
 
+"""
+X X X X X
+X O O O X
+X O C O X
+X O O O X
+X X X X X 
+"""
+def large_square_shape(x, y, tile=[]):
+    toggle(x, y, tile)              #center
+    toggle(x - 1, y, tile)          #left
+    toggle(x + 1, y, tile)          #right
+    toggle(x, y - 1, tile)          #upper center
+    toggle(x - 1, y - 1, tile)      #upper left
+    toggle(x + 1, y - 1, tile)      #upper right
+    toggle(x + 1, y + 1, tile)      #lower right
+    toggle(x, y + 1, tile)          #lower center
+    toggle(x - 1, y + 1, tile)      #lower left
 
 def toggle(x, y, tile=[]):
     if tile[x][y] == "X":
