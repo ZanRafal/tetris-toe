36dedc54e7caff4179a35f2fc704a6d2fa2a9351
ZanRafal <01159225@pw.edu.pl>
2021-12-27 19:11:09 +0100

    Inkarnacja geniuszu

--- Tile.py
+++ Tile.py
@@ -1,7 +1,15 @@
 class Tile:
 
-    def __init__(self, x, y, isMutable):
+    def __init__(self, x, y, isImmutable, attribute):
         self.x = x
         self.y = y
-        self.isMutable = isMutable
+        self.isMutable = isImmutable
+        self.attribute = attribute
 
+def toggle(self):
+    if self.isImmutable == True: return
+
+    if self.attribute == "X":
+        self.attribute = "O"
+    elif self.attribute == "O":
+        self.attribute = "X"
