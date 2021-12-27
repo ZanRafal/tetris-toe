36e6460dee4bac1f22aaafd535ac89ef4ed0430b
ZanRafal <01159225@pw.edu.pl>
2021-12-27 18:01:04 +0100

    Dodano metodę zamiany w kształcie prostokąta

--- Shapes.py
+++ Shapes.py
@@ -1,6 +1,19 @@
 def set1(x, y):
     print("XD")
 
+"""
+X X X X X
+X O O O X
+X O C O X
+X X X X X 
+"""
+def square_shape(x, y, tile=[]):
+    toggle(x, y, tile)              #lower center
+    toggle(x - 1, y, tile)          #lower left
+    toggle(x + 1, y, tile)          #lower right
+    toggle(x, y - 1, tile)          #upper center
+    toggle(x - 1, y - 1, tile)      #upper left
+    toggle(x + 1, y - 1, tile)      #upper right
 
 def toggle(x, y, tile=[]):
     if tile[x][y] == "X":
