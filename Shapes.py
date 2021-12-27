fcd9a163d16c8699110f3be43a538a1d7378755e
ZanRafal <01159225@pw.edu.pl>
2021-12-27 18:05:56 +0100

    Dodano metodę zamiany w kształcie małego kwadratu

--- Shapes.py
+++ Shapes.py
@@ -7,13 +7,26 @@ X O O O X
 X O C O X
 X X X X X 
 """
-def square_shape(x, y, tile=[]):
+def rectangle_shape(x, y, tile=[]):
     toggle(x, y, tile)              #lower center
     toggle(x - 1, y, tile)          #lower left
     toggle(x + 1, y, tile)          #lower right
     toggle(x, y - 1, tile)          #upper center
     toggle(x - 1, y - 1, tile)      #upper left
     toggle(x + 1, y - 1, tile)      #upper right
+
+"""
+X X X X
+X O O X
+X C O X
+X X X X 
+"""
+def small_square_shape(x, y, tile=[]):
+    toggle(x, y, tile)              #lower center
+    toggle(x + 1, y, tile)          #lower right
+    toggle(x, y - 1, tile)          #upper left
+    toggle(x + 1, y - 1, tile)      #upper right
+
 
 def toggle(x, y, tile=[]):
     if tile[x][y] == "X":
