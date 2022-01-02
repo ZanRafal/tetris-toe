609559d72c705036858f9038c34d6b6063b4c37e
ZanRafal <01159225@pw.edu.pl>
2022-01-02 21:47:10 +0100

    Dodano logikę wyświetlania czasu, i wyniku

--- TimerLogic.py
+++ TimerLogic.py
@@ -8,9 +8,10 @@ def calculate_score(total_time, total_moves):
     pass
 
 
-def display_score_and_time():
-    pass
+def display_final_time_and_moves(moves_count, timer):
+    print("Finalna liczba ruchów: " + moves_count)
+    print("Całkowity czas rozgrywki: " + timer)
 
 
 def display_final_score():
-    pass
+    print("Twój końcowy wynik to: " + calculate_score() + '\n')
