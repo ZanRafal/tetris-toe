4dcabf759163348cd929be6438f94b2bb31f063c
ZanRafal <01159225@pw.edu.pl>
2022-01-03 19:38:50 +0100

    Zaimplementowanie logiki końca gry

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Rafał Zan
 # 311 214
-
+import _thread
 import Board
 import Shapes
+import time
+from Timer import Timer
 from modules.getChar import *
-
 
 def main():
     game_board = Board.init_board(8)
@@ -14,19 +15,20 @@ def main():
 def play(my_board):
     moves_count = 0
     row = col = 4
-    new_figure = Shapes.get_shape()
+    new_figure = Shapes.get_shape(1)
     Board.display_board(my_board.copy())
-    while True:
+    while Board.number_of_x(my_board) != 0:
         ch = get_nonblock_char(0.5)
         if ch != '':
             row, col = change_position(ch, row, col)
             if ch == '\n':
                 moves_count += 1
                 Shapes.choose_from_set(row, col, new_figure, my_board)
-                new_figure = Shapes.get_shape()
+                new_figure = Shapes.get_shape(1)
                 row = col = 4
             Board.update_display(row, col, new_figure, my_board)
             display_current_moves_count(moves_count)
+    display_win_message()
 
 
 def change_position(var, row, col):
