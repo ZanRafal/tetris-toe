7de0a9168fef13be151acd5eae0f66449dbe1676
ZanRafal <01159225@pw.edu.pl>
2022-01-04 19:47:49 +0100

    Dodano logikę menu głównego gry

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import TimerLogic as tl
 import Board
 import Shapes
 from modules.getChar import *
-
+import MenuLogic as ml
 
 def main():
-    game_board = Board.init_board(8, 8)
-    play(game_board)
+    print(ml.get_difficulty_level())
+
+
+
+    # game_board = Board.init_board(8, 8)
+    # play(game_board)
     # menu()
 
 
@@ -66,36 +70,6 @@ def display_time(value):
         print('Czas rozgrywki: {} : 0{}'.format(minutes, seconds))
     else:
         print('{} : {}'.format(minutes, seconds))
-
-
-# def menu():
-#     os.system('clear')
-#     print("Select game options:")
-#     print('[1]Play game \n'
-#           '[2]Leaderboards \n'
-#           '[3]Settings \n'
-#           '[4]Exit \n'
-#           )
-#     val = input()
-#     if val == 1:
-#         #play()
-#         print("Play")
-#         pass
-#     elif val == 2:
-#         print("leaderboards")
-#         #show_leaderboards()
-#     elif val == 3:
-#         print("settings")
-#         #show_game_settings()
-#     elif val == 4:
-#         exit(0)
-#     else:
-#         print("Podano nieprawidłową wartość:")
-#         menu()
-#
-# def show_game_settings():
-#     pass
-
 
 if __name__ == "__main__":
     main()
