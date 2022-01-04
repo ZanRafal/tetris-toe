49095f00f20ea341b0dbd854006f97cc7a162221
ZanRafal <01159225@pw.edu.pl>
2022-01-04 21:10:48 +0100

    Dodano sygnaturę metody save to leaderboards

--- LeaderboardsLogic.py
+++ LeaderboardsLogic.py
@@ -1,16 +1,34 @@
 ##Leaderboards
+import os
+import MenuLogic
+
+
 
 def show_leaderboards():
+    os.system('clear')
+    print("Tablice wyników")
+
+
+    exit =  input()
+    print("[1]Wyjście")
+    if exit == '1':
+        MenuLogic.menu()
+
+
+def save_to_leaderboards(player_name, game_time, number_of_moves,  player_score):
+    buffer = player_name + " " + str(number_of_moves) + " " + str(game_time) + " " + str(player_score)
+    if capacity_not_exceeded():
+        file = open("leaderboards_db.txt", "a+")
+        file.write(buffer)
+        file.close()
+
+
+def capacity_not_exceeded():
     pass
-
-
-def save_to_leaderboards():
-    pass
-
 
 def read_from_file():
     pass
 
 
 def save_to_file():
-    pass
+    file = open("leaderboards_db.txt", "w+")
