fd24a0687657cdbd81f8350dff12fdac43082386
ZanRafal <01159225@pw.edu.pl>
2022-01-06 11:51:35 +0100

    Dodano wartości stałe w słownikach

--- Dictionaries.py
+++ Dictionaries.py
@@ -2,3 +2,7 @@ Difficulty = {1: 'Łatwy',
               2: 'Średni',
               3: 'Trudny'
               }
+
+WIN_MESSAGE = "Gratulacje!! Udało Ci się ukończyć grę!"
+EXIT_MESSAGE = "Do zobaczenia następnym razem :D "
+INVALID_VALUE = "Podano nieprawidłową wartość!"
