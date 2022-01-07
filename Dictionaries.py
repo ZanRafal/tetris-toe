c7b28b04453a07a439104fd292654a552e5c5181
ZanRafal <01159225@pw.edu.pl>
2022-01-07 23:10:02 +0100

    Dodano ścieżkę do application.properties

--- Dictionaries.py
+++ Dictionaries.py
@@ -6,3 +6,12 @@ Difficulty = {1: 'Łatwy',
 WIN_MESSAGE = "Gratulacje!! Udało Ci się ukończyć grę!"
 EXIT_MESSAGE = "Do zobaczenia następnym razem :D "
 INVALID_VALUE = "Podano nieprawidłową wartość!"
+
+
+#Application.properties
+app_properties = "app__properties.txt"
+
+#database file paths
+easy_db = "difficulty/easy_leaderboards_db.txt"
+medium_db = "difficulty/easy_leaderboards_db.txt"
+hard_db = "difficulty/easy_leaderboards_db.txt"
